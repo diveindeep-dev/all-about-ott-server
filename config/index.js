441ae4007b123e8aee8cdbcbbe375c0c7c7cf7c8
hakyung <diveindeep.dev@gmail.com>
2023-01-29 14:21:32 +0900

    Connect to MongoDB with mongoose

--- config/index.js
+++ config/index.js
@@ -1,4 +1,13 @@
-const CLIENT_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export { CLIENT_URL };
+const CLIENT_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : process.env.PRODUCT_URL;
+
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/db';
+
+const PORT = process.env.PORT || 3001;
+
+export { CLIENT_URL, DB_URL, PORT };
